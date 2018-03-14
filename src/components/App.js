0667918379a23b71e1f7eca0c5be2617efb1df8d
gabrielamf <glennygabb@gmail.com>
2018-03-14 10:15:55 -0500

    corrige ruta imagen

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Section from './Section';
 
-const img ='foto.png';
+const img ='../../public/foto.jpg';
 const hobbies =['hobbie 1','hobbie 2' ,'hobbie 3'];
 const text = 'cualquier texto';
 
